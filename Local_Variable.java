87ec09a8556cab1d4e9813e1f740c345cc10f71a
Satya <raghuani2000@gmail.com>
2025-08-16 23:22:01 +0530

    Add files via upload

---
+++ Local_Variable.java
@@ -0,0 +1,8 @@
+public class Local_Variable
+    {
+        public static void main (String Args[])
+            {
+                int a = 20;
+                System.out.println(a);
+            }
+    }
